03eec8858d6fc2507a0f03aa2d3907663e54139d
iramnazz <iramnaz948@gmail.com>
2026-01-20 07:59:58 +0530

    home page added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export function Home(){
+    return (
+        <>
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+           {/* theme toggle */}
+           {/* background effects */}
+            {/* <StarBackground/> */}
+           {/* navbar */}
+           {/* main content */}
+            <main>
+                
+            </main>
+           {/* footer */}
+        </div>
+        </>
+    )
+}
